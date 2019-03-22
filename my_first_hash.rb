473ba9e2818fc273d83c9cdd8cfd1af9ad35b2dd
Sarah Doherty Granoff <sdgranoff@gmail.com>
2019-03-22 19:51:01 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -11,8 +11,8 @@ def shipping_manifest
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
 
   the_manifest = {
-    "whale bone corsets" => 5
-    "porcelain vases" => 3
+    "whale bone corsets" => 5,
+    "porcelain vases" => 3,
     "oil paintings" => 2
   }
 
